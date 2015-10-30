4c8ed0da2023b87c1d58ccbceef74cc508bdf6de
jiumogaoao <394127821@qq.com>
2015-10-30 16:59:38 +0800

    change
    change

--- model/formTable.js
+++ model/formTable.js
@@ -5,6 +5,7 @@
 		css:["form_table"],
 		html:["form_table"],
 		fn:function(){
+			var result={};
 			var data={};
 			/*
 			var data={
@@ -34,11 +35,51 @@
 			source.reflash=function(){
 				var main=_.template(source.html[0])(data);
 				source.target.html(source.css[0]+main);
+				source.target.find("[D_type='input']").unbind("change").bind("change",function(){
+					result[$(this).attr("D_key")]=$(this).val();
+					});
+				source.target.find("[D_type='select'] .dropdownPoint").unbind("click").bind("click",function(){
+					result[$(this).parents(".select").attr("D_key")]=$(this).attr("value");
+					result[$(this).parents(".select").find(".value").html($(this).html());
+					});
+				source.target.find("[D_type='number'] .add").unbind("click").bind("click",function(){
+					var newValue=1;
+					if(result[$(this).parents(".number").attr("D_key")]){
+						newValue=result[$(this).parents(".number").attr("D_key")]+1;
+						}
+					result[$(this).parents(".number").attr("D_key")]=newValue;
+					$(this).parents(".number").find(".value").val(newValue);
+					});
+				source.target.find("[D_type='number'] .sub").unbind("click").bind("click",function(){
+					var newValue=0;
+					if(result[$(this).parents(".number").attr("D_key")]&&result[$(this).parents(".number").attr("D_key")]>0){
+						newValue=result[$(this).parents(".number").attr("D_key")]-1;
+						}
+					result[$(this).parents(".number").attr("D_key")]=newValue;
+					$(this).parents(".number").find(".value").val(newValue);
+					});
+				source.target.find("[D_type='number'] .value").unbind("change").bind("change",function(){
+					result[$(this).parents(".number").attr("D_key")]=$(this).val();
+					});
+				source.target.find("[D_type='time']").each(function(){
+					var that=this;
+					$(this).datepicker({
+						change:function(){
+							result[$(that).attr("D_key")]=$(that).val();
+							}
+});
+					});
 				};
 			//set
 			source.set=function(setData){
 				data=setData;
 				};
+			source.setResult=function(setData){
+				result=setData;
+				}
+			source.result=function(){
+				return result;
+				}
 			}
 		});
 	})($,app,config);
